bf604ea1f163968136fb45969867443da24cfed1
Fabo2201 <167098660+Fabo2201@users.noreply.github.com>
2024-08-02 19:51:16 -0300

    Add files via upload

---
+++ camcelv4/src/pages/Soporte.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+         <div class="flex flex-center"><ChatRobado/>
+    </div>
+    </div>
+</template>
+<script setup>
+import ChatRobado from "src/components/ChatRobado.vue"
+</script>
+<style scoped>
+div{
+    height: 100vh;
+    background-color: antiquewhite;
+}
+</style>
